ca4973a96dc7d7edf2b8ba948900edb50b0e0705
Kerynn Davis <kerynn.davis@gmail.com>
2022-11-14 08:15:03 -0800

    feat: Add visitor class with functionality

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,3 +1,24 @@
 class Visitor
+  attr_reader :name,
+              :height,
+              :preferences
 
+  def initialize(name, height, spending_money)
+    @name = name
+    @height = height
+    @spending_money = spending_money
+    @preferences = []
+  end
+
+  def spending_money
+    @spending_money.delete('$').to_i
+  end
+
+  def add_preferences(preference)
+    @preferences << preference
+  end
+
+  def tall_enough?(threshold)
+    @height >= threshold
+  end
 end
---
+++ spec/visitor_spec.rb
@@ -0,0 +1,44 @@
+require 'rspec'
+require './lib/visitor'
+
+describe Visitor do
+  it 'exists' do
+    visitor1 = Visitor.new('Bruce', 54, '$10')
+
+    expect(visitor1).to be_a(Visitor)
+  end
+
+  it 'has a name, height, and spending money' do
+    visitor1 = Visitor.new('Bruce', 54, '$10')
+
+    expect(visitor1.name).to eq('Bruce')
+    expect(visitor1.height).to eq(54)
+    expect(visitor1.spending_money).to eq(10)
+  end
+
+  it 'has preferences that starts as an empty array' do
+    visitor1 = Visitor.new('Bruce', 54, '$10')
+
+    expect(visitor1.preferences).to eq([])
+  end
+
+  it 'can add preferences' do
+    visitor1 = Visitor.new('Bruce', 54, '$10')
+
+    visitor1.add_preferences(:gentle)
+    visitor1.add_preferences(:water)
+
+    expect(visitor1.preferences).to eq([:gentle, :water])
+  end
+
+  it 'can determine if visitor tall enough based on given threshold' do
+    visitor1 = Visitor.new('Bruce', 54, '$10')
+    visitor2 = Visitor.new('Tucker', 36, '$5')
+    visitor3 = Visitor.new('Penny', 64, '$15')
+
+    expect(visitor1.tall_enough?(54)).to be true
+    expect(visitor2.tall_enough?(54)).to be false
+    expect(visitor3.tall_enough?(54)).to be true
+    expect(visitor1.tall_enough?(64)).to be false
+  end
+end
